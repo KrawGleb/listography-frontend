889250ab14e2ead86d07d1313cf3d88d7c4a0e6f
KrawGleb <krawcevitsch@gmail.com>
2022-09-29 21:04:02 +0300

    Make up html structure

--- src/app/modules/components/components.module.ts
+++ src/app/modules/components/components.module.ts
@@ -7,10 +7,12 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
