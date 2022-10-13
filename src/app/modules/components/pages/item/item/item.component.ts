d6f2b437bc822d027138b9c435721a1c79b5b0eb
KrawGleb <krawcevitsch@gmail.com>
2022-10-13 14:22:03 +0300

    Add comments and like

--- src/app/modules/components/pages/item/item/item.component.ts
+++ src/app/modules/components/pages/item/item/item.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { map, takeUntil, tap } from 'rxjs';
 import { getRandomColor } from 'src/app/helpers/random-color.helper';
+import { CommentModel } from 'src/app/models/comment.model';
 import { Item } from 'src/app/models/item.model';
 import { DestroyableComponent } from 'src/app/modules/shared/helpers/destroyable/destroyable.component';
 import { ListsService } from 'src/app/modules/shared/services/api/lists.service';
+import { SocialService } from 'src/app/modules/shared/services/api/social.service';
 
 @Component({
   selector: 'app-item',
@@ -15,10 +18,16 @@ export class ItemComponent extends DestroyableComponent {
   private id!: number;
 
   public item?: Item;
+  public preparedColors: string[] = [];
+  public comments: CommentModel[] = [];
+  public commentsForm = new FormGroup({
+    comment: new FormControl(''),
+  });
 
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly listsService: ListsService
+    private readonly listsService: ListsService,
+    private readonly socialService: SocialService
   ) {
     super();
 
@@ -29,10 +38,54 @@ export class ItemComponent extends DestroyableComponent {
       .pipe(
         takeUntil(this.onDestroy$),
         map((response) => response.body),
-        tap((item) => (this.item = item))
+        tap((item) => {
+          this.item = item;
+          this.comments = item.comments;
+
+          this.preparedColors = item.tags.map(() => getRandomColor());
+        })
       )
       .subscribe();
   }
 
   public getRandomColor = getRandomColor;
+
+  public likeButtonClick() {
+    if (!this.item) return;
+
+    this.item.liked ? this.dislike() : this.like();
+  }
+
+  public commentButtonClick() {
+    const content = this.commentsForm.value.comment;
+
+    if (content) {
+      this.socialService
+        .comment(this.id, content)
+        .pipe(takeUntil(this.onDestroy$))
+        .subscribe();
+    }
+
+    this.commentsForm.controls.comment.setValue('');
+  }
+
+  private like() {
+    this.item!.liked = true;
+    this.item!.totalLikesCount! += 1;
+
+    this.socialService
+      .like(this.item?.id!)
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe();
+  }
+
+  private dislike() {
+    this.item!.liked = false;
+    this.item!.totalLikesCount! -= 1;
+
+    this.socialService
+      .dislike(this.item?.id!)
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe();
+  }
 }
