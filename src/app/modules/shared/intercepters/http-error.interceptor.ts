e0e083bd4f7b1a8e5a8e31f5c0d82a71dc27c2b4
KrawGleb <krawcevitsch@gmail.com>
2022-10-23 12:28:29 +0300

    Fix snackbar error message

--- src/app/modules/shared/intercepters/http-error.interceptor.ts
+++ src/app/modules/shared/intercepters/http-error.interceptor.ts
@@ -6,12 +6,25 @@ import {
   HttpInterceptor,
   HttpErrorResponse,
 } from '@angular/common/http';
-import { catchError, Observable, of, retry, throwError } from 'rxjs';
+import { catchError, Observable, retry } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { LocalStorageConstants } from 'src/app/models/constants/local-storage.constants';
+import { Router } from '@angular/router';
+import { ErrorResponse } from 'src/app/models/responses/error-response.model';
+
+const errorMessages = {
+  0: "Server is not available",
+  401: "Unauthorize",
+  403: "Forbidden",
+  404: "Font found",
+  500: "Internal server error. Please, try again later"
+}
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
-  constructor(private readonly snackbar: MatSnackBar) {}
+  constructor(
+    private readonly snackbar: MatSnackBar,
+    private readonly router: Router) {}
 
   intercept(
     request: HttpRequest<unknown>,
@@ -22,10 +35,23 @@ export class HttpErrorInterceptor implements HttpInterceptor {
       catchError((error: HttpErrorResponse) => {
         let errorMessage = '';
 
-        if (error.error instanceof ErrorEvent) {
-          errorMessage = `Error: ${error.error.message}`;
-        } else {
-          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        const status = error.status;
+
+
+        if ((errorMessages as any)[status]) {
+          errorMessage = (errorMessages as any)[status];
+        }
+
+        console.log((error.error as any).errors as ErrorResponse);
+
+        if ((error.error as any).errors) {
+          errorMessage = (error.error as any).errors.join('\n');
+        }
+
+
+        if (status === 401 || status === 403) {
+          localStorage.removeItem(LocalStorageConstants.Token);
+          this.router.navigateByUrl('/login');
         }
 
         this.snackbar.open(errorMessage, 'Ok', {
