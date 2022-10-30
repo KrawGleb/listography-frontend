a94da024a2eb1ab8a90f3362d82f90d594d9f5a8
KrawGleb <krawcevitsch@gmail.com>
2022-10-30 04:11:23 +0300

    Store selected language and theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostBinding } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { LocalStorageConstants } from './models/constants/local-storage.constants';
 import { AuthService } from './modules/auth/services/auth.service';
 import { SignalRService } from './modules/shared/services/common/signal-r.service';
@@ -17,23 +18,40 @@ export class AppComponent {
   constructor(
     private readonly signalRService: SignalRService,
     private readonly authService: AuthService,
-    private readonly router: Router) {
+    private readonly router: Router,
+    private readonly translateService: TranslateService
+  ) {
+    this.initLanguage();
+    this.initTheme();
+
     this.signalRService.startConnection();
     this.signalRService.addListener('logout_user', (username: string) => {
       const currentUser = localStorage.getItem(LocalStorageConstants.Username);
 
-      console.log(currentUser);
-
-      if (currentUser === username)
-      {
+      if (currentUser === username) {
         this.authService.logout();
         this.router.navigateByUrl('/login');
       }
-    })
-
+    });
   }
 
   public onThemeChanged(isDarkMode: boolean) {
     this.className = isDarkMode ? this.darkModeClassName : '';
+  }
+
+  private initTheme() {
+    const theme = localStorage.getItem(LocalStorageConstants.Theme);
+
+    if (theme) {
+      this.onThemeChanged(theme === 'dark');
+    }
+  }
+
+  private initLanguage() {
+    const language = localStorage.getItem(LocalStorageConstants.Language);
+
+    if (language) {
+      this.translateService.use(language);
+    }
   }
 }
